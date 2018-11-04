37649be6e5ff1671499e546f149081e80ab770bc
Anonranger <anonranger@protonmail.com>
2018-11-04 05:28:34 -0800

    Multiplication Tables

---
+++ ninjamath_mt.py
@@ -0,0 +1,23 @@
+i = 1
+j = 1
+mr = 1
+while i <= 12:
+    print ("Multiplication Table :"+str(i))
+    while j <= 12:
+        mr = i * j
+        #print(i)
+        #print(j)
+        #print(mr)
+        print(str(i)+"*"+str(j)+"="+str(mr))
+        j = j + 1
+    print("\n")
+    i = i + 1
+    j = 1
+
+
+
+
+
+
+
+
