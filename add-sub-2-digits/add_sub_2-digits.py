5592f81698831c418cdbc6e36946ed764da093fa
Prakash Sankar <prakashsankar@protonmail.com>
2018-11-07 09:26:58 -0800

    11/7

---
+++ add-sub-2-digits/add_sub_2-digits.py
@@ -0,0 +1,24 @@
+import random
+k = 1
+print("ADD AND SUBTRACT TWO DIGIT NUMBERS:")
+print("/n")
+while (k <= 30):
+    i = random.randint(10,99)
+    j = random.randint(1,99)
+    operator = ['+', '-']
+    co=random.choice(operator)
+    print(str(i)+" "+str(co)+" "+str(j)+" "+"=")
+    print("/n")
+    print("/n")
+    print("/n")
+    k = k + 1
+
+    
+
+
+
+
+
+
+
+
